f790a7809dbd60c32bfa9eed0322874fa98ae407
Maciej Stranz <01150920@pw.edu.pl>
2024-06-16 22:27:06 +0200

    fixed dataset

--- forecast/split.py
+++ forecast/split.py
@@ -3,10 +3,12 @@ from sklearn.model_selection import train_test_split
 
 # Wczytaj dane z pliku CSV
 file_path = 'Data\weatherHistory.csv'
-data = pd.read_csv(file_path)
+df = pd.read_csv(file_path)
 
-# Podział na zbiór treningowy i testowy w proporcji 80:20
-train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+# Załóżmy, że mamy DataFrame df
+train_size = int(0.8 * len(df))  # 80% na trening
+train_data = df[:train_size]
+test_data = df[train_size:]
 
 # Zapisz wyniki do plików CSV
 train_data.to_csv('Data/weather_train_raw.csv', index=False)
