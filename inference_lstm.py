6dd2fd6fad8dfaa17a6846596bb753817526fcfb
Maciej Stranz <01150920@pw.edu.pl>
2024-06-21 01:15:37 +0200

    improve interference

--- inference_lstm.py
+++ inference_lstm.py
@@ -17,19 +17,33 @@ def calculate_rmse(true_future, predicted_future):
 def inverse_standardize(scaled_data, mean, std):
     return (scaled_data * std) + mean
 
-def multi_step_plot(history, true_future, prediction):
-  plt.figure(figsize=(12, 6))
-  num_in = create_time_steps(len(history))
-  num_out = len(true_future)
+# def multi_step_plot(history, true_future, prediction):
+#   plt.figure(figsize=(12, 6))
+#   num_in = create_time_steps(len(history))
+#   num_out = len(true_future)
 
-  plt.plot(num_in, np.array(history[:, 1]), label='History')
-  plt.plot(np.arange(num_out)/STEP, np.array(true_future), 'bo',
-           label='True Future')
-  if prediction.any():
-    plt.plot(np.arange(num_out)/STEP, np.array(prediction), 'ro',
-             label='Predicted Future')
-  plt.legend(loc='upper left')
-  plt.show()
+#   plt.plot(num_in, np.array(history[:, 0]), label='History')
+#   plt.plot(np.arange(num_out)/STEP, np.array(true_future), 'bo',
+#            label='True Future')
+#   if prediction.any():
+#     plt.plot(np.arange(num_out)/STEP, np.array(prediction), 'ro',
+#              label='Predicted Future')
+#   plt.legend(loc='upper left')
+#   plt.show()
+
+def multi_step_plot(history, true_future, prediction, num_points=6):
+    plt.figure(figsize=(12, 6))
+    num_in = create_time_steps(len(history))
+    num_out = len(true_future)
+
+    plt.plot(num_in, np.array(history[:, 1]), label='History')
+    plt.plot(np.arange(num_points)/STEP, np.array(true_future[:num_points]), 'bo',
+             label='True Future')
+    if prediction.any():
+        plt.plot(np.arange(num_points)/STEP, np.array(prediction[:num_points]), 'ro',
+                 label='Predicted Future')
+    plt.legend(loc='upper left')
+    plt.show()
 
 def multivariate_data(dataset, target, start_index, end_index, history_size,
                       target_size, step, single_step=False):
@@ -54,9 +68,9 @@ def multivariate_data(dataset, target, start_index, end_index, history_size,
 def create_time_steps(length):
   return list(range(-length, 0))
 
-hrs = 8
-dataset = 'jenaClimate'
-target = 'temp'
+hrs = 24
+dataset = 'weatherHistory'
+target = 'humidity'
 
 if dataset == 'jenaClimate':
     csv_path = "Data\jena_climate_2009_2016.csv"
@@ -65,7 +79,14 @@ if dataset == 'jenaClimate':
     features = df[features_considered]
     features.index = df['Date Time']
     TRAIN_SPLIT = 300000
+    if target == 'temp':
+       desired_target = 1
+    elif target == 'humidity':
+       desired_target = -1
+    else:
+       print('wrong target')
     model_path = "Models/lstm_" + str(target) +"_"+ str(hrs) + "hrs_jena.keras"
+    points_to_show = hrs*6
 elif dataset == 'weatherHistory':
     csv_path = "Data\weatherHistory.csv"
     df = pd.read_csv(csv_path)
@@ -74,10 +95,21 @@ elif dataset == 'weatherHistory':
     features = df[features_considered]
     features.index = df['Formatted Date']
     TRAIN_SPLIT = 70000
+    if target == 'temp':
+       desired_target = 0
+    elif target == 'humidity':
+       desired_target = -1
+    else:
+       print('wrong target')
     model_path = "Models/lstm_" + str(target) +"_"+ str(hrs) + "hrs_weather.keras"
+    points_to_show = hrs
 else:
   print("Provide valid dataset")
 
+
+# dataset = features.values
+# data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+# data_std = dataset[:TRAIN_SPLIT].std(axis=0)
 
 dataset = features.values
 data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
@@ -89,17 +121,11 @@ past_history = 720
 STEP = 6
 future_target = hrs*6
 
-x_train_multi, y_train_multi = multivariate_data(dataset, dataset[:, 1], 0,
-                                                TRAIN_SPLIT, past_history,
-                                                future_target, STEP)
-x_val_multi, y_val_multi = multivariate_data(dataset, dataset[:, 1],
+x_val_multi, y_val_multi = multivariate_data(dataset, dataset[:, desired_target],
                                             TRAIN_SPLIT, None, past_history,
                                             future_target, STEP)
 BATCH_SIZE = 256
 BUFFER_SIZE = 10000
-
-train_data_multi = tf.data.Dataset.from_tensor_slices((x_train_multi, y_train_multi))
-train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
 
 val_data_multi = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
 val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
@@ -111,7 +137,7 @@ model = load_model(model_path)
 all_true_futures = []
 all_predicted_futures = []
 
-for x, y in val_data_multi.take(len(x_val_multi) // BATCH_SIZE):
+for x, y in val_data_multi.take(len(x_val_multi) // BATCH_SIZE//10):
     predictions = model.predict(x)
     
     for i in range(len(x)):
@@ -119,8 +145,8 @@ for x, y in val_data_multi.take(len(x_val_multi) // BATCH_SIZE):
         predicted_future = predictions[i]
 
         # Odwrócenie standaryzacji
-        true_future_real = inverse_standardize(true_future, data_mean[1], data_std[1])
-        predicted_future_real = inverse_standardize(predicted_future, data_mean[1], data_std[1])
+        true_future_real = inverse_standardize(true_future, data_mean[desired_target], data_std[desired_target])
+        predicted_future_real = inverse_standardize(predicted_future, data_mean[desired_target], data_std[desired_target])
 
         all_true_futures.append(true_future_real)
         all_predicted_futures.append(predicted_future_real)
@@ -130,8 +156,15 @@ all_true_futures = np.concatenate(all_true_futures, axis=0)
 all_predicted_futures = np.concatenate(all_predicted_futures, axis=0)
 
 # Obliczanie zbiorczych metryk
+
 overall_mae = calculate_mae(all_true_futures, all_predicted_futures)
 overall_rmse = calculate_rmse(all_true_futures, all_predicted_futures)
 
 print(f'Overall MAE: {overall_mae}')
 print(f'Overall RMSE: {overall_rmse}')
+
+for x, y in val_data_multi.take(5):
+  multi_step_plot(inverse_standardize(x[desired_target],data_mean[desired_target], data_std[desired_target]), 
+                  inverse_standardize(y[desired_target],data_mean[desired_target], data_std[desired_target]),
+                  inverse_standardize(model.predict(x)[desired_target],data_mean[desired_target], data_std[desired_target]),
+                  num_points=points_to_show)
